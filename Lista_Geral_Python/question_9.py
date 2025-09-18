f1090f91363061cf716008808ed75f5b7cd2f55e
Pedro <110909102+kaserzin@users.noreply.github.com>
2025-09-18 16:24:50 -0300

    1 ao 10

---
+++ Lista_Geral_Python/question_9.py
@@ -0,0 +1,7 @@
+###  Escreva um programa que leia o valor de uma distância percorrida em km e o tempo 
+# gasto em horas e calcule a velocidade média em m/s.
+
+distancia = int(input("Informe a distancia percorrida: "))
+tempo = int(input(f"Informe o tempo em que percorreu {distancia}km: "))
+print(f"A distacia percorrida foi: {distancia}\nFoi percorrida em: {tempo}h\nEm uma velocidade de {(distancia / tempo):.1f}km/h\nEm m/s fica: {((distancia / tempo) / 3.6):.2f}m/s")
+
