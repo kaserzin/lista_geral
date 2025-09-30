57eb742e531eda3c902651fe20b547e889fae6f3
Pedro <110909102+kaserzin@users.noreply.github.com>
2025-09-30 17:23:49 -0300

    ate 37c

---
+++ Lista_Geral_C/comandos de entrada e saida/question37.c
@@ -0,0 +1,24 @@
+//Escreva um programa que leia um número positivo entre 1 e 9999 e calcule o valor da
+//unidade de milhar, da centena, da dezena e da unidade.
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+
+    int numero;
+
+    printf("Informe um numero positivo entre 1 e 9999: ");
+    scanf("%d", &numero);
+
+    int unidade_milhar = numero / 1000;
+    int centena = (numero / 100) % 10;
+    int dezena = (numero % 100) / 10;
+    int unidade = numero % 10;
+
+    printf("unidade_milhar = %d\n", unidade_milhar);
+    printf("centena = %d\n", centena);
+    printf("dezena = %d\n", dezena);
+    printf("unidade = %d", unidade);
+
+    return 0;
+}
